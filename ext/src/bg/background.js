4c366695a85ab66d7e5183ec789d948228f245e6
Cole Chamberlain <cole.chamberlain@gmail.com>
2015-06-16 18:04:19 -0700

    got JS working better

--- ext/src/bg/background.js
+++ ext/src/bg/background.js
@@ -1,9 +1,3 @@
-// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
-
-// var settings = new Store("settings", {
-//     "sample_setting": "This is how you use Store.js to remember values"
-// });
-
 
 //example of using a message handler from the inject scripts
 chrome.extension.onMessage.addListener(
@@ -11,3 +5,12 @@ chrome.extension.onMessage.addListener(
   	chrome.pageAction.show(sender.tab.id);
     sendResponse();
   });
+
+chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+  if (changeInfo.status == 'complete' && tab.active) {
+
+    chrome.tabs.executeScript(null, {file: "src/inject/inject.js"});
+    // do your things
+
+  }
+});
